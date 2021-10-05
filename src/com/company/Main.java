50aa033570f16051ed3eef9072fd5a423fbbdebc
ДашаY13 <dasha130289@rambler.ru>
2021-10-05 21:27:16 +0300

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int balance = 2_000_000_000 ;
+        int translation = 500_000_000;
+        int finite = balance + translation;
+        System.out.println(finite);
+    }
+}
